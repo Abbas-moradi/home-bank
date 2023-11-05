fa3bb879c62c93ff705bbcab72c73b4cb9b10b95
Abbas moradi <en.moradi.66@gmail.com>
2023-11-05 20:34:21 +0330

    implement user forms

---
+++ HomeBank/accounts/forms.py
@@ -0,0 +1,37 @@
+from typing import Any
+from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from accounts.models import User
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('national_code', 'full_name', 'phone', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password1'] and cd['password2'] and cd['password1'] != cd['password2']:
+            raise ValidationError('password dont match')
+        return cd['password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.changed_data['password2'])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(help_text='you can change password using <a href="../password/">this form.</a>')
+    fields = ('national_code',
+              'full_name',
+              'phone',
+              'email',
+              'password',
+              'last_login')
