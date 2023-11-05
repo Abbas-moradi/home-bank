8bca00dd04993ed4dc5d34ea939a2521fe917d86
Abbas moradi <en.moradi.66@gmail.com>
2023-11-05 21:29:17 +0330

    implement accounts admin.py

--- HomeBank/accounts/admin.py
+++ HomeBank/accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from accounts.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import Group
+from accounts.models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('national_code', 'full_name', 'phone', 'is_admin')
+    list_filter = ('is_admin', )
+
+    fieldsets = (
+        (None, {'fields': ('national_code', 'full_name', 'phone', 'email', 'password')}),
+        ('permission', {'fields': ('is_admin', 'is_active', 'last_login')}),
+    )
+    add_fieldsets = ('national_code', 'full_name', 'phone')
+    ordering = ('join_date', )
+    filter_horizontal = ()
+
+
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
