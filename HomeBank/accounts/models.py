706ba65b4079f211010a1a19fee798f276ac3819
Abbas moradi <en.moradi.66@gmail.com>
2023-11-05 19:35:06 +0330

    implement user models

--- HomeBank/accounts/models.py
+++ HomeBank/accounts/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+import uuid
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    id = models.UUIDField(uuid.uuid4, editable=False, unique=True, primary_key=True)
+    national_code = models.CharField(max_length=10, unique=True)
+    full_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=13, unique=True)
+    email = models.EmailField(max_length=200, unique=True)
+    sex = models.CharField(max_length=10, choices=[
+        ('M', 'men'),
+        ('W', 'women')
+    ])
+    photo = models.ImageField(upload_to='media/%Y/%m/%d/')
+    join_date = models.DateField(auto_now_add=True)
+    
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    directorship = models.BooleanField(default=False)
+    is_inspector = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'national_code'
+    REQUIRED_FIELDS = ['full_name', 'phone', 'email']
+
+    objects = ''
+
+    def __str__(self) -> str:
+        return self.national_code
+    
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_madule_perms(self, app_lable):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin
+
