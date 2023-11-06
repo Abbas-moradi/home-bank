6957905d3ba231c597b0b92d8299defc56f46b34
Abbas moradi <en.moradi.66@gmail.com>
2023-11-06 23:52:01 +0330

    admin site registration LoanTransaction models

--- HomeBank/loan/admin.py
+++ HomeBank/loan/admin.py
@@ -5,4 +5,10 @@ from loan.models import Loan, LoanTransaction
 @admin.register(Loan)
 class LoanAdmin(admin.ModelAdmin):
     list_display = ['account', 'start_date', 'loan_amount', 'end_date']
-    
+
+
+@admin.register(LoanTransaction)
+class LoanTransactionAdmin(admin.ModelAdmin):
+    list_display = ['transaction', 'loan', 'amount_installment', 
+                    'date', 'status', 'loan_balance']
+
