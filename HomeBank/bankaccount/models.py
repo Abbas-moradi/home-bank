423c0d5a929c28d52ad58e4be4e8b172de5639cd
Abbas moradi <en.moradi.66@gmail.com>
2023-11-06 13:32:18 +0330

    implement account models in bank account

--- HomeBank/bankaccount/models.py
+++ HomeBank/bankaccount/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_number = models.PositiveIntegerField()
+    balance = models.IntegerField()
+    opening_date = models.DateField(auto_now_add=True)
+    loan_status = models.BooleanField(default=False)
+    status = models.BooleanField(default=True)
+    closed = models.BooleanField(default=False)
+    closed_date = models.DateField(null=True, blank=True)
+
+    def set_closed_date(self):
+        from datetime import date
+        if self.closed:
+            self.closed_date = date.today()
+        else:
+            self.closed_date = None
+    
+
