55254959b7cd0ce7f4472eea0efc4c72688f3360
Abbas moradi <en.moradi.66@gmail.com>
2023-11-06 23:39:07 +0330

    implement loan models and meta classes

--- HomeBank/loan/models.py
+++ HomeBank/loan/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from bankaccount.models import Account
 
-# Create your models here.
+
+class Loan(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    start_date = models.DateField(auto_now_add=True)
+    loan_amount = models.IntegerField()
+    end_date = models.DateField(null=True)
+    wag_amount = models.IntegerField()
+    term = models.IntegerField()
+    installment_number = models.PositiveSmallIntegerField()
+    delay_days = models.PositiveSmallIntegerField(default=0)
+    installment_paid = models.IntegerField()
+    status = models.BooleanField(default=True)
+    clearing = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Loan'
+        verbose_name_plural = 'Loans'
+        ordering = ('start_date', )
+    
+    def __str__(self) -> str:
+        return self.id
