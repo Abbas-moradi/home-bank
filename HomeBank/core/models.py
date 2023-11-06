f7792923171c101367f5f67f4ceef66589b34de1
Abbas moradi <en.moradi.66@gmail.com>
2023-11-06 13:02:47 +0330

    implement branch models

--- HomeBank/core/models.py
+++ HomeBank/core/models.py
@@ -17,3 +17,18 @@ class BranchSetting(models.Model):
         vebose_name = 'setting'
         verbose_name_plural = 'Setting'
         ordering = 'id'
+
+
+class Branch(models.Model):
+    salary = models.PositiveIntegerField(default=0)
+    count = models.PositiveIntegerField(default=0)
+    cost = models.PositiveIntegerField(default=0)
+    wage = models.PositiveIntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.id
+    
+    class Meta:
+        verbose_name = 'branch'
+        verbose_plural_name = 'branch'
+        ordering = 'id'
