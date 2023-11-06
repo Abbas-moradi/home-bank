595adbddd5ac7f9b86f1ac1264e1a16d2f0419c1
Abbas moradi <en.moradi.66@gmail.com>
2023-11-07 00:23:46 +0330

    implement serializers (drf)

--- HomeBank/accounts/views.py
+++ HomeBank/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from accounts.serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from accounts.models import User
 
-# Create your views here.
+
+class UserApiView(APIView):
+    def get(self):
+        users = User.objects.all()
+        ser_data = UserSerializer(instance=users, many=True)
+        return Response(ser_data.data, status=status.HTTP_200_OK)
