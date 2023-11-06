a20d085eb71e5075b15516cb520cabb7de5b10ef
Abbas moradi <en.moradi.66@gmail.com>
2023-11-06 12:53:34 +0330

    implement branch setting models

--- HomeBank/core/models.py
+++ HomeBank/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class BranchSetting(models.Model):
+    salary = models.IntegerField(default=0)
+    Installment_number = models.PositiveSmallIntegerField(default=1)
+    percent = models.DecimalField(default=1.0)
+    bank_email = models.EmailField(max_length=250)
+    bank_card_number = models.CharField(max_length=16)
+    description = models.TextField()
+    sms_fee = models.PositiveSmallIntegerField(default=250)
+
+    def __str__(self) -> str:
+        return self.id
+    
+    class Meta:
+        vebose_name = 'setting'
+        verbose_name_plural = 'Setting'
+        ordering = 'id'
