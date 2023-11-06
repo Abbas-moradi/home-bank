2dd37d3b7d1204bc7ac458ef52e54e7256fd1da5
Abbas moradi <en.moradi.66@gmail.com>
2023-11-06 23:19:40 +0330

    BranchSetting models admin site registration

--- HomeBank/core/admin.py
+++ HomeBank/core/admin.py
@@ -6,3 +6,9 @@ from core.models import Branch, BranchSetting
 class BranchAdmin(admin.ModelAdmin):
     list_display = ['salary', 'count', 'cost', 'wage']
 
+
+@admin.register(BranchSetting)
+class BranchSettingAdmin(admin.ModelAdmin):
+    list_display = ['tution', 'Installment_number', 
+                    'percent', 'bank_email', 'bank_card_number',
+                    'description', 'sms_fee']
