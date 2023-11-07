1c1ab80fe4b785fe64db2b0a7c111dc074673a3b
Abbas moradi <en.moradi.66@gmail.com>
2023-11-07 19:02:56 +0330

    implement user register serializer

--- HomeBank/accounts/serializers.py
+++ HomeBank/accounts/serializers.py
@@ -12,4 +12,13 @@ class UserSerializer(serializers.ModelSerializer):
             'password': {'write_only': True}
         }
 
-    
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        models = User
+        fields = ('national_code', 'full_name', 'phone', 'email', 'password', 'password2')
+        extra_kwargs = {
+            'password':{'write_only':True},
+            }
