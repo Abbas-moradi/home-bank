27d83879fd1143b787c323d4cd0876156f810adf
Abbas moradi <en.moradi.66@gmail.com>
2023-11-07 22:22:09 +0330

    improve serializers user creation code

--- HomeBank/accounts/serializers.py
+++ HomeBank/accounts/serializers.py
@@ -18,16 +18,25 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('national_code', 'full_name', 'phone', 'email', 'password', 'password2')
+        fields = ('national_code', 'full_name', 'phone', 'email', 'sex', 'password', 'password2')
         extra_kwargs = {
             'password':{'write_only':True},
             }
         
     def validate(self, data):
-        if data['password'] != data['password2']:
-            raise serializers.ValidationError('password confirm dont match.')
-        if 'admin' in data['email']:
-            raise serializers.ValidationError('`admin` not in email.')
         if 'admin' in data['full_name']:
-            raise serializers.ValidationError('`admin` not in full name.')
+            raise serializers.ValidationError('Admin cannot be in full name')
+        if 'admin' in data['email']:
+            raise serializers.ValidationError('Admin cannot be in email')
+        if len(data['national_code']) != 10:
+            raise serializers.ValidationError('national code must be 10 number length.')
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError('Password verification does not match.')
+        if 'sex' in ('M', 'W'):
+            raise serializers.ValidationError('Gender(sex) must be M or W')
         return data
+    
+    def create(self, validated_data):
+        del validated_data['password2']
+        print(validated_data)
+        return User.objects.create(**validated_data)
--- HomeBank/accounts/views.py
+++ HomeBank/accounts/views.py
@@ -18,12 +18,6 @@ class UserRegisterApiView(APIView):
     def post(self, request):
         ser_data = UserRegisterSerializer(data=request.POST)
         if ser_data.is_valid():
-            User.objects.create(
-                national_code = ser_data.validated_data['national_code'],
-                email = ser_data.validated_data['email'],
-                full_name = ser_data.validated_data['full_name'],
-                phone = ser_data.validated_data['phone'],
-                password = ser_data.validated_data['password'],
-            )
+            ser_data.create(ser_data.validated_data)
             return Response(ser_data.data, status=status.HTTP_201_CREATED)
         return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
