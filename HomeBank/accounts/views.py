a964badcc075b4e03a8a99bdfb187350e949c54c
Abbas moradi <en.moradi.66@gmail.com>
2023-11-07 19:10:45 +0330

    implement user register api view

--- HomeBank/accounts/views.py
+++ HomeBank/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from accounts.serializers import UserSerializer
+from accounts.serializers import UserSerializer, UserRegisterSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from accounts.models import User
@@ -11,3 +11,19 @@ class UserApiView(APIView):
         users = User.objects.all()
         ser_data = UserSerializer(instance=users, many=True)
         return Response(ser_data.data, status=status.HTTP_200_OK)
+    
+
+class UserRegisterApiView(APIView):
+
+    def post(self, request):
+        ser_data = UserRegisterSerializer(data=request.POST)
+        if ser_data.is_valid():
+            User.objects.create(
+                national_code = ser_data.validated_data['national_code'],
+                email = ser_data.validated_data['email'],
+                full_name = ser_data.validated_data['full_name'],
+                phone = ser_data.validated_data['phone'],
+                password = ser_data.validated_data['password'],
+            )
+            return Response(ser_data.data, status=status.HTTP_201_CREATED)
+        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
