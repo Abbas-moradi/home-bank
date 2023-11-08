d9ea86be3b5c1a3d52eac76abd377179498493b8
Abbas moradi <en.moradi.66@gmail.com>
2023-11-08 13:33:56 +0330

    improve the view classes Separation of activities

--- HomeBank/accounts/urls.py
+++ HomeBank/accounts/urls.py
@@ -5,7 +5,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('users/', UserApiView.as_view(), name='users'),
-    path('register/', UserApiView.as_view(), name='register'),
-    path('update/<str:pk>/', UserApiView.as_view(), name='update'),
-    path('delete/<str:pk>/', UserApiView.as_view(), name='delete'),
+    path('register/', UserCreateApiView.as_view(), name='register'),
+    path('update/<str:pk>/', UserUpdateApiView.as_view(), name='update'),
+    path('delete/<str:pk>/', UserDeleteApiView.as_view(), name='delete'),
 ]
--- HomeBank/accounts/views.py
+++ HomeBank/accounts/views.py
@@ -7,21 +7,26 @@ from accounts.models import User
     
 
 class UserApiView(APIView):
-    """ In this class you can view and create and 
-    update or delete user by primary key"""
+    """ In this urls you can view the all users """
 
     def get(self, request):
         users = User.objects.all()
         ser_data = UserSerializer(instance=users, many=True)
         return Response(ser_data.data, status=status.HTTP_200_OK)
 
+
+class UserCreateApiView(APIView):
+    """ In this urls you can created user """
     def post(self, request):
         ser_data = UserRegisterSerializer(data=request.POST)
         if ser_data.is_valid():
             ser_data.create(ser_data.validated_data)
             return Response(ser_data.data, status=status.HTTP_201_CREATED)
         return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+
+class UserUpdateApiView(APIView):
+    """  In this urls you can update user """
     def put(self, request, pk):
         user = User.objects.get(pk=pk)
         ser_data = UserUpdateSerializer(instance=user, data=request.data, partial=True)
@@ -29,7 +34,11 @@ class UserApiView(APIView):
             ser_data.save()
             return Response(ser_data.data, status=status.HTTP_200_OK)
         return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+
+class UserDeleteApiView(APIView):
+    """ In this urls you can theleted user, 
+        when deleted user, user is_active field equal False """
     def delete(self, request, pk):
         user = User.objects.get(pk=pk)
         if user.is_active == False:
