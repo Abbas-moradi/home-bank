1988b7d9f983a9f8321024353c1f2d8f99f610e3
Abbas moradi <en.moradi.66@gmail.com>
2023-11-11 20:05:46 +0330

    implementation loan create api view

--- HomeBank/loan/urls.py
+++ HomeBank/loan/urls.py
@@ -7,4 +7,5 @@ app_name = 'loan'
 
 urlpatterns = [
     path('loans/', LoansApiView.as_view(), name='loans'),
+    path('createloan/', LoanCreateApiView.as_view(), name='createloan'),
 ]
--- HomeBank/loan/views.py
+++ HomeBank/loan/views.py
@@ -4,6 +4,9 @@ from loan.models import Loan
 from loan.serializers import LoanSerializers
 from rest_framework.response import Response
 from rest_framework import status
+from bankaccount.models import Account
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import login, logout
 
 
 class LoansApiView(APIView):
@@ -12,5 +15,20 @@ class LoansApiView(APIView):
         loans = Loan.objects.filter(status=True, termination=False)
         ser_data = LoanSerializers(instance=loans, many=True)    
         return Response(ser_data.data, status=status.HTTP_200_OK)
-       
+    
+class LoanCreateApiView(APIView):
+    
+    def post(self, request):
+        account = get_object_or_404(Account, pk=request.POST['account'],
+                                    status=True, closed=False)
+        if account.loan_status == False:
+            return Response({'result': 'account have loan'})
+    
+        loan_instance = Loan(account=account)
+        loan_instance.save()
+        account.loan_status = False
+        account.save()
+        return Response({'status':'loan paid'}, 
+                        status=status.HTTP_201_CREATED)
+
 
