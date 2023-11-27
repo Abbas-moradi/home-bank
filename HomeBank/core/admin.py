11ea1e59dd76f5c2b2f52e5e0706dd9b644fe25a
Abbas moradi <en.moradi.66@gmail.com>
2023-11-27 23:18:55 +0330

    model request register in admin site

--- HomeBank/core/admin.py
+++ HomeBank/core/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
-from core.models import Branch, BranchSetting
+from core.models import Branch, BranchSetting, Request
 
 
 @admin.register(Branch)
 class BranchAdmin(admin.ModelAdmin):
-    list_display = ['salary', 'count', 'cost', 'wage']
+    list_display = ('salary', 'count', 'cost', 'wage')
 
 
 @admin.register(BranchSetting)
 class BranchSettingAdmin(admin.ModelAdmin):
-    list_display = ['tution', 'loan_amount', 'installment_number', 
+    list_display = ('tution', 'loan_amount', 'installment_number', 
                     'wage', 'bank_email', 'bank_card_number',
-                    'description', 'sms_fee']
+                    'description', 'sms_fee')
+    
+
+@admin.register(Request)
+class RequestAdmin(admin.ModelAdmin):
+    list_display = ('request', 'description', 'created', 'seen', 'user')
